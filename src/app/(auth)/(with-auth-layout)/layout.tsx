ab06a3a75d0bb08b1564c70a35130e539f14f451
siihasann <hasannajib3@gmail.com>
2025-02-09 15:27:53 +0700

    Active Links

--- src/app/(auth)/(with-auth-layout)/layout.tsx
+++ src/app/(auth)/(with-auth-layout)/layout.tsx
@@ -1,11 +1,32 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import "./styles.css";
+
+const navLinks = [
+    {name: "Register", href: "/register"},
+    {name: "Login", href: "/login"},
+]
+
 export default function AuthLayout({
     children,
 }: {
     children: React.ReactNode
-}) {
+    }) {
+    const pathname = usePathname();
+
     return (
         <div>
-            <h2>Inner layout</h2>
+            {navLinks.map((link) => {
+                const isActive = pathname === link.href;
+                return (
+                    <Link key={link.name} href={link.href}
+                        className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"}>
+                        {link.name}
+                    </Link>
+                );
+            })}
             {children}
         </div>
     );
